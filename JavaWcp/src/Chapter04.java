3a471cfc0c001c975ad2abbde28d9b367741de70
savasan2 <83535755+savasan2@users.noreply.github.com>
2021-09-15 06:15:56 +0900

    chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String num1 = "Ishida";
+		System.out.println(num1);
+		num1 = "Ringo";
+		System.out.println(num1);
+		int LuckyNumber = 7;
+		System.out.println(LuckyNumber);
+	}
+}
