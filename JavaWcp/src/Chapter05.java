b42addebac85c24fc4f9f0e1a82668fb691a1b65
savasan2 <83535755+savasan2@users.noreply.github.com>
2021-09-16 23:58:03 +0900

    chapter5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,20 @@
+
+ public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123L;
+		System.out.println(long1);
+		double double1 = 123;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		Float float1 = 123f;
+		System.out.println(float1);
+		Integer int1 = 123;
+		System.out.println(int1);
+		Character char1 = 'あ';
+		System.out.println(char1);
+		int[] array1 = {0,1,2,3,4};
+		System.out.println(array1[3]);
+	}
+
+}
